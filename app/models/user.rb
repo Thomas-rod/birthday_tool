1042e6d36bf7fb7dbf265b672a915e91309adef7
thomas rodier <rodiert17@gmail.com>
2020-06-29 09:40:44 +0200

    update scheduler

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,33 +18,37 @@ class User < ApplicationRecord
 
   def send_reminder_previous_day
     self.all.each do |user|
-      tomorow_birthdays = []
       tomorow_birthdays = user.birthdays.select{ |b| b.start.day == (Date.today.day + 1) && b.start.month == Date.today.month && b.start.year == Date.today.year && b.friend.reminder_previous_day == true }
-      UserMailer.with(user: user, tomorow_birthdays: tomorow_birthdays).send_reminders_previous_day.deliver_now
+      unless tomorow_birthdays.empty?
+        UserMailer.with(user: user, tomorow_birthdays: tomorow_birthdays).send_reminders_previous_day.deliver_now
+      end
     end
   end
 
   def send_reminders_today_morning
     self.all.each do |user|
-      today_birthdays = []
       today_birthdays = user.birthdays.select{ |b| b.start.day == Date.today.day && b.start.month == Date.today.month && b.start.year == Date.today.year && b.friend.reminder_today_morning == true }
-      UserMailer.with(user: user, today_birthdays: tomorow_birthdays).send_reminders_today_morning.deliver_now
+      unless today_birthdays.empty?
+        UserMailer.with(user: user, today_birthdays: today_birthdays).send_reminders_today_morning.deliver_now
+      end
     end
   end
 
   def send_reminders_today_noon
     self.all.each do |user|
-      tomorow_birthdays = []
-      tomorow_birthdays = user.birthdays.select{ |b| b.start.day == Date.today.day && b.start.month == Date.today.month && b.start.year == Date.today.year && b.friend.reminder_today_noon == true }
-      UserMailer.with(user: user, tomorow_birthdays: tomorow_birthdays).send_reminders_today_noon.deliver_now
+      today_birthdays = user.birthdays.select{ |b| b.start.day == Date.today.day && b.start.month == Date.today.month && b.start.year == Date.today.year && b.friend.reminder_today_noon == true }
+      unless today_birthdays.empty?
+        UserMailer.with(user: user, today_birthdays: today_birthdays).send_reminders_today_noon.deliver_now
+      end
     end
   end
 
   def send_reminders_today_night
     User.all.each do |user|
-      tomorow_birthdays = []
-      tomorow_birthdays = user.birthdays.select{ |b| (b.start.day) == (Date.today.day) && b.start.month == Date.today.month && b.start.year == Date.today.year && b.friend.reminder_today_night == true }
-      UserMailer.with(user: user, tomorow_birthdays: tomorow_birthdays).send_reminders_today_night.deliver_now
+      today_birthdays = user.birthdays.select{ |b| (b.start.day) == (Date.today.day) && b.start.month == Date.today.month && b.start.year == Date.today.year && b.friend.reminder_today_night == true }
+      unless today_birthdays.empty?
+        UserMailer.with(user: user, today_birthdays: today_birthdays).send_reminders_today_night.deliver_now
+      end
     end
   end
 
